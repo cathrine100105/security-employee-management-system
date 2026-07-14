25d6dc5194c81836e84bae25655e06e0e0b32027
jinettashree <jinettashree@gmail.com>
2026-07-14 10:48:02 +0530

    Fix API Base URL

--- admin-dashboard/src/services/axiosInstance.js
+++ admin-dashboard/src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://security-employee-management-system-2.onrender.com",
+  baseURL: "https://security-employee-management-system-2.onrender.com/api",
 });
 
 export default api;
