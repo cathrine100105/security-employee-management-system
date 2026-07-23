278f5e220b1792cea132a14238fcf404da6b65ed
cathrine100105 <cathrine.peterraj@gmail.com>
2026-07-23 09:19:32 +0530

    Configure Brevo SMTP using environment variables

--- management/src/main/java/com/security/management/email/EmailService.java
+++ management/src/main/java/com/security/management/email/EmailService.java
@@ -1,6 +1,8 @@
 package com.security.management.email;
 
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,6 +12,15 @@ import org.springframework.stereotype.Service;
 public class EmailService {
 
     private final JavaMailSender mailSender;
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private String port;
+
+    @Value("${spring.mail.username}")
+    private String username;
 
     public void sendResetPasswordEmail(String toEmail, String resetLink) {
 
@@ -23,6 +34,10 @@ public class EmailService {
                             "Click the link below:\n\n" +
                             resetLink
             );
+            System.out.println("Host: " + host);
+            System.out.println("Port: " + port);
+            System.out.println("Username: " + username);
+
 
             mailSender.send(message);
             System.out.println("Email sent successfully.");
