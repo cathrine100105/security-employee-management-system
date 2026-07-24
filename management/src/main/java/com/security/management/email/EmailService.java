ad094cc130829618679c9651f172011f8eeffc54
jinettashree <jinettashree@gmail.com>
2026-07-24 21:00:19 +0530

    Update Email Service

--- management/src/main/java/com/security/management/email/EmailService.java
+++ management/src/main/java/com/security/management/email/EmailService.java
@@ -34,16 +34,24 @@ public class EmailService {
 
         message.setText(
                 "Hello,\n\n" +
-                        "Click below:\n\n" +
-                        resetLink
+                "You requested to reset your password.\n\n" +
+                "Click the link below to reset your password:\n\n" +
+                resetLink +
+                "\n\nThis link will expire in 15 minutes.\n\n" +
+                "If you didn't request this, please ignore this email.\n\n" +
+                "Regards,\n" +
+                "Security Employee Management System"
         );
 
         try {
             mailSender.send(message);
-            System.out.println("Email sent successfully");
+            System.out.println("Email sent successfully.");
         } catch (Exception e) {
+            System.out.println("Email sending failed.");
             e.printStackTrace();
         }
-
     }
+
+
 }
+ 
