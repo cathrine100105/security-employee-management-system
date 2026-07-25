50f3fb7663a0940231d8cdfb3c5d1b4b35856b92
cathrine100105 <cathrine.peterraj@gmail.com>
2026-07-25 06:37:56 +0530

    Updated Email Service

--- management/src/main/java/com/security/management/email/EmailService.java
+++ management/src/main/java/com/security/management/email/EmailService.java
@@ -25,33 +25,38 @@ public class EmailService {
     public void sendResetPasswordEmail(String toEmail, String resetLink) {
 
         System.out.println("Sending email to: " + toEmail);
+
         System.out.println("Reset Link: " + resetLink);
 
         SimpleMailMessage message = new SimpleMailMessage();
 
         message.setTo(toEmail);
+
         message.setSubject("Reset Your Password");
 
         message.setText(
+
                 "Hello,\n\n" +
-                "You requested to reset your password.\n\n" +
-                "Click the link below to reset your password:\n\n" +
-                resetLink +
-                "\n\nThis link will expire in 15 minutes.\n\n" +
-                "If you didn't request this, please ignore this email.\n\n" +
-                "Regards,\n" +
-                "Security Employee Management System"
+
+                        "Click below:\n\n" +
+
+                        resetLink
+
         );
 
         try {
+
             mailSender.send(message);
-            System.out.println("Email sent successfully.");
+
+            System.out.println("Email sent successfully");
+
         } catch (Exception e) {
-            System.out.println("Email sending failed.");
+
             e.printStackTrace();
+
         }
+
     }
 
 
 }
- 
